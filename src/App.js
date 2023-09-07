54866a22982e31653e77fbd5d4f172f988fea2ce
rameezraja0 <93797020+rameezraja0@users.noreply.github.com>
2023-09-08 01:14:23 +0500

    8 sept Night
    Install react router dom
    managed it
    fav icon and index html setting
    add some buttons for multiple dark modes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-// import Alert from './components/Alert';
-// import About from './components/About';
+import Alert from './components/Alert';
+import About from './components/About';
 import React, {useState} from 'react'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState('light');
   const [alert, setAlert] = useState(null);
-  const [redmode, setredMode] = useState('light');
+  const [redmode, setredMode] = useState('light'); // red mode usestate
 
 
   const showAlert=(message, type)=>{
@@ -25,40 +31,49 @@ function App() {
       setMode('dark')
       document.body.style.backgroundColor= '#042743';
       showAlert("Dark mode is enabled", "success");
+      document.title="Rameezistan - Dark Mode"
     }
     else{
       setMode('light')
       document.body.style.backgroundColor= 'white';
       showAlert("Light mode is enabled", "success");
+      document.title="Rameezistan - Light Mode"
 
     }
   }
 
   // for red mode
   const redtoggleMode = ()=>{
-    if(redmode === 'light'){
-      setredMode('danger')
+    if(redmode === 'danger'){
+      setredMode('light')
       document.body.style.backgroundColor= 'White';
       showAlert("Light mode is enabled", "success");
+      document.title="Rameezistan - Light Mode"
+
     }
     else{
-      setredMode('light')
+      setredMode('danger')
       document.body.style.backgroundColor= '#ee6b6e';
       showAlert("Red mode is enabled", "success");
+      document.title="Rameezistan - Red Mode"
+
 
     }
   }
-
+// end of red mode
 
   return(
     <>
+    <Router>
 <Navbar title='Rameezistan' mode={mode} toggleMode={toggleMode} redtoggleMode={redtoggleMode}/>
-{/* <Alert alert={alert}/> */}
-
-<div className="container my-3">
-    <TextForm showAlert={showAlert} heading="Enter something to analyze" mode={mode} />
-    {/* <About /> */}
-</div>
+<Alert alert={alert}/>
+    <div className="container my-3">
+    <Routes>
+              <Route exact path="/about" element={<About />} />
+              <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter something to analyze" mode={mode} />}/>
+    </Routes>
+    </div>
+    </Router>
     </>
   );
 }
