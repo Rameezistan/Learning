0e21796702bdc68b8c35a2907752e81cbe18dc35
rameezraja0 <93797020+rameezraja0@users.noreply.github.com>
2023-09-10 05:46:36 +0500

    main updated

--- src/App.js
+++ src/App.js
@@ -2,21 +2,20 @@ import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
-// // import About from './components/About';
-import React, {useState} from 'react'
-// import {
-//   BrowserRouter as Router,
-//   Routes,
-//   Route
-// } from "react-router-dom";
+import About from './components/About';
+import React, { useState } from 'react'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState('light');
   const [alert, setAlert] = useState(null);
-  const [redmode, setredMode] = useState('light'); // red mode usestate
 
 
-  const showAlert=(message, type)=>{
+  const showAlert = (message, type) => {
     setAlert({
       msg: message,
       type: type
@@ -25,56 +24,31 @@ function App() {
       setAlert(null);
     }, 1500);
   }
-  const toggleMode = ()=>{
-    if(mode === 'light'){
+  const toggleMode = () => {
+    if (mode === 'light') {
       setMode('dark')
-      document.body.style.backgroundColor= '#042743';
+      document.body.style.backgroundColor = '#042743';
       showAlert("Dark mode is enabled", "success");
-      document.title="Rameezistan - Dark Mode"
     }
-    else{
+    else {
       setMode('light')
-      document.body.style.backgroundColor= 'white';
+      document.body.style.backgroundColor = 'white';
       showAlert("Light mode is enabled", "success");
-      document.title="Rameezistan - Light Mode"
 
     }
   }
-
-  // for red mode
-  const redtoggleMode = ()=>{
-    if(redmode === 'danger'){
-      setredMode('light')
-      document.body.style.backgroundColor= 'White';
-      showAlert("Light mode is enabled", "success");
-      document.title="Rameezistan - Light Mode"
-
-    }
-    else{
-      setredMode('danger')
-      document.body.style.backgroundColor= '#ee6b6e';
-      showAlert("Red mode is enabled", "success");
-      document.title="Rameezistan - Red Mode"
-
-
-    }
-  }
-// end of red mode
-
-  return(
+  return (
     <>
-    {/* <Router> */}
-<Navbar title='Rameezistan' mode={mode} toggleMode={toggleMode} redtoggleMode={redtoggleMode}/>
-<Alert alert={alert}/>
-   <div className="container my-3">
-    {/*  <Routes>
-              <Route exact path="/about" element={<About />} /> */}
-              {/* <Route exact path="/" element={ }>*/}
-
-              <TextForm showAlert={showAlert} heading="Enter something to analyze" mode={mode} />
-    {/* </Routes> */}
-    </div>
-    {/*</Router> */} 
+      <Router>
+        <Navbar title='Rameezistan' mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            <Route exact path="/about" element={<About mode={mode} />} />
+            <Route exact path="/" element={<TextForm showAlert={showAlert} heading=" Try WordCounter, Character Counter, Remove Extra spaces" mode={mode} />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,58 +1,59 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 
 export default function TextForm(props) {
-  const handleupclick = ()=>{
+  const handleupclick = () => {
     let newtext = text.toUpperCase();
     setText(newtext)
     props.showAlert("Converted to Uppercase", "success")
   }
 
-  const handleloclick = ()=>{
+  const handleloclick = () => {
     let newtext = text.toLowerCase();
     setText(newtext)
     props.showAlert("Converted to Lowercase", "success")
 
   }
-  const handleupchange = (event)=>{
+  const handleupchange = (event) => {
     setText(event.target.value);
   }
 
-  const handlecopy = ()=>{
-    var text = document.getElementById("myBox");
-    text.select();
-    navigator.clipboard.writeText(text.value);
+  const handlecopy = () => {
+    navigator.clipboard.writeText(text);
     props.showAlert("Copied to Clipboard!", "success")
 
   }
-
-  const extraSpace = ()=>{
+  const extraSpace = () => {
     let newText = text.split(/[ ]+/);
     setText(newText.join(' '))
     props.showAlert("Extra spaces removed", "success")
 
   }
-   
+
   const [text, setText] = useState('');
   return (
     <>
-    <div className='container' style={{color: props.mode==='dark'?'white':'black'}}>
-        <h1>{props.heading}</h1>
+      <div className='container' style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
+        <h1 className='mb-4'>{props.heading}</h1>
         <div className="mb-3">
-        <textarea className="form-control" placeholder='Enter Text here' value={text} style={{backgroundColor: props.mode==='dark'?'grey':'white', color: props.mode==='dark'?'white':'black'}} onChange={handleupchange} id="myBox" rows="8"></textarea>
+          <textarea className="form-control" value={text} style={{
+            backgroundColor: props.mode === 'dark' ? '#13466e' : 'white',
+            color: props.mode === 'dark' ? 'white' : 'black'
+          }} onChange={handleupchange} id="myBox" rows="8"></textarea>
         </div>
-        <button className="btn btn-primary mx-1"  onClick={handleupclick}>Convert to Uppercase</button>
-        <button className="btn btn-primary mx-1" onClick={handleloclick}>Convert to lowercase</button>
-        <button className="btn btn-primary mx-1" onClick={handlecopy}>Copy Text</button>
-        <button className="btn btn-primary mx-1" onClick={extraSpace}>Remove Extra Spaces</button>
-    </div>
-    <div className="container my-3" style={{color: props.mode==='dark'?'white':'black'}}>
-      <h2>Your text summary</h2>
-      <p>{text.split(" ").length} words and {text.length} characters</p>
-      <p>{0.008* text.split(" ").length} Minutes Read</p>
-      <h3>Preview</h3>
-      <p>{text.length>0?text:"Enter something to Preview"}</p>
-    </div>
+        <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={handleupclick}>Convert to Uppercase</button>
+        <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={handleloclick}>Convert to lowercase</button>
+        <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={handlecopy}>Copy Text</button>
+        <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={extraSpace}>Remove Extra Spaces</button>
+      </div>
+      <div className="container my-3" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
+        <h2>Your text summary</h2>
+        <p>{text.split(/\s+/).filter((element) => { return element.length !== 0 }).length} words and {text.length} characters</p>
+        <p>{0.008 * text.split(" ").filter((element) => { return element.length !== 0 }).length} Minutes Read</p>
+        <h3>Preview</h3>
+        <p>{text.length > 0 ? text : "Enter something to Preview"}</p>
+
+      </div>
     </>
   )
 }
